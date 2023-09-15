5f7d3d45f22c8b30e653c5d880608076020e0be5
Sarvesh23923 <sarvesh23923@gmail.com>
2023-09-15 15:05:56 +0530

    first commit

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: avoid_print, prefer_typing_uninitialized_variables, await_only_futures
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fCMToken = await _firebaseMessaging.getToken();
+    print('Token: $fCMToken');
+    FirebaseMessaging.onBackgroundMessage((handleBackgroundMessage));
+  }
+}
